28d16e5fff44433d22514c0928a53ba38f18217d
Luis <lumihernadezma@ittepic.edu.mx>
2021-06-08 22:12:34 -0700

    08/06/2021 LMHM
    Se modifico la interfaz equipo y materia, asi como sus .php y se actualizo el sql de la base de datos

--- BD_GPS.sql
+++ BD_GPS.sql
@@ -1,7 +1,23 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     19/05/2021 20:56:18                          */
+/* Created on:     02/06/2021 21:37:35                          */
 /*==============================================================*/
+
+/*==============================================================*/
+/* Table: CATALOGO_PRESUPUESTO                                  */
+/*==============================================================*/
+create table CATALOGO_PRESUPUESTO
+(
+   ID_PRESUPUESTO       varchar(15)  comment '',
+   NUMERO               int  comment '',
+   NORMA                varchar(50)  comment '',
+   CONCEPTO_SERVICIO    varchar(250)  comment '',
+   UNIDAD               varchar(50)  comment '',
+   CANTIDAD             int  comment '',
+   PU                   varchar(50)  comment '',
+   IMPORTE_LETRA        varchar(250)  comment '',
+   IMPORTE_NUMERO       float  comment ''
+);
 
 /*==============================================================*/
 /* Table: CAT_PRESTACION                                        */
@@ -9,9 +25,19 @@
 create table CAT_PRESTACION
 (
    IDCATPRESTACION      int not null auto_increment  comment '',
-   IDPRESTACION         int  comment '',
    NOMCATPRESTACION     varchar(30) not null  comment '',
    primary key (IDCATPRESTACION)
+);
+
+/*==============================================================*/
+/* Table: COSTOMANTENER                                         */
+/*==============================================================*/
+create table COSTOMANTENER
+(
+   IDGASTO              int  comment '',
+   ID_MANTENIMIENTO     int  comment '',
+   CODIGO_EQUIPO        varchar(16)  comment '',
+   MONTO_MANTENER       float  comment ''
 );
 
 /*==============================================================*/
@@ -20,7 +46,6 @@ create table CAT_PRESTACION
 create table EQUIPO
 (
    CODIGO_EQUIPO        varchar(16) not null  comment '',
-   CANTIDAD_EQUIPO      int not null  comment '',
    DESCRIPCION_EQUIPO   varchar(100)  comment '',
    CARACTERISTICAS      varchar(100)  comment '',
    MARCA_EQUIPO         varchar(50)  comment '',
@@ -75,12 +100,23 @@ create table GASTOS_MATERIAL
 );
 
 /*==============================================================*/
+/* Table: GASTOS_SEGURO                                         */
+/*==============================================================*/
+create table GASTOS_SEGURO
+(
+   IDGASTO              int  comment '',
+   ID_SEGURO            varchar(50)  comment '',
+   MONTO_SEGURO         float  comment ''
+);
+
+/*==============================================================*/
 /* Table: GASTOS_SERVICIO                                       */
 /*==============================================================*/
 create table GASTOS_SERVICIO
 (
    IDSERVICIO           int  comment '',
-   IDGASTO              int  comment ''
+   IDGASTO              int  comment '',
+   PRECIOUNITARIOSERVICIO float not null  comment ''
 );
 
 /*==============================================================*/
@@ -93,6 +129,8 @@ create table INCIDENTES
    IDTRABAJADOR         int  comment '',
    DESCRIPCION_INCIDENTE varchar(100) not null  comment '',
    FECHA_INCIDENTE      date not null  comment '',
+   PETICION_FINANCIEROS varchar(2)  comment '',
+   IMG_TICKET           varchar(200)  comment '',
    primary key (ID_INCIDENTE)
 );
 
@@ -163,10 +201,26 @@ create table INGRESOS
 (
    IDINGRESO            int not null auto_increment  comment '',
    ID_OBRA              varchar(50)  comment '',
+   IDTRABAJADOR         int  comment '',
    MONTO_INGRESO        float not null  comment '',
    FECHAINGRESO         date not null  comment '',
    DESCRIPCIONINGRESO   varchar(100) not null  comment '',
    primary key (IDINGRESO)
+);
+
+/*==============================================================*/
+/* Table: MANTENIMIENTO                                         */
+/*==============================================================*/
+create table MANTENIMIENTO
+(
+   ID_MANTENIMIENTO     int not null auto_increment  comment '',
+   CODIGO_EQUIPO        varchar(16)  comment '',
+   PROVEEDOR            varchar(50)  comment '',
+   FECHA_PROX_M         date  comment '',
+   ESTADO               varchar(20)  comment '',
+   OBSERVACIONES        varchar(100)  comment '',
+   TIPO_SERVICIO        char(1)  comment '',
+   primary key (ID_MANTENIMIENTO)
 );
 
 /*==============================================================*/
@@ -179,6 +233,7 @@ create table MATERIAL
    CANTIDAD_MATERIAL    int not null  comment '',
    UNIDAD_MEDIDA        varchar(50) not null  comment '',
    DESCRIPCION          varchar(100) not null  comment '',
+   FECHA_VENCIDO        date  comment '',
    primary key (ID_MATERIAL)
 );
 
@@ -201,6 +256,10 @@ create table NOMINA
    FECHA                date not null  comment '',
    TOTALPAGO            float not null  comment '',
    OBSPAGO              varchar(200)  comment '',
+   INICIOPERIODO        date not null  comment '',
+   FINPERIODO           date not null  comment '',
+   PERCEPCIONES         float not null  comment '',
+   DESCUENTOS           float not null  comment '',
    primary key (IDPAGO)
 );
 
@@ -210,8 +269,10 @@ create table NOMINA
 create table PRESTACIONES
 (
    IDPRESTACION         int not null auto_increment  comment '',
+   IDCATPRESTACION      int  comment '',
    NOMBREPRES           varchar(30) not null  comment '',
    DESCRIPCIONPRES      varchar(100) not null  comment '',
+   VALORMUL             float not null  comment '',
    primary key (IDPRESTACION)
 );
 
@@ -224,7 +285,7 @@ create table PRESTACION_NOMINA_TRAB
    IDPRESTACION         int  comment '',
    IDPAGO               int  comment '',
    IDTRABAJADOR         int  comment '',
-   VALOR                float not null  comment '',
+   MONTO                float  comment '',
    primary key (IDPRESNOMTRAB)
 );
 
@@ -237,6 +298,8 @@ create table PRESTAMOS
    IDTRABAJADOR         int  comment '',
    MONTO                float not null  comment '',
    RESTANTE             float not null  comment '',
+   FECHAPRESTAMO        date not null  comment '',
+   ESTADOPRESTAMO       char(1)  comment '',
    primary key (IDPRESTAMO)
 );
 
@@ -260,6 +323,7 @@ create table PUESTOS
    IDPUESTOS            int not null auto_increment  comment '',
    NOMBREPUESTO         varchar(50) not null  comment '',
    DESCRIPCIONPUESTO    varchar(200)  comment '',
+   SUELDO               float  comment '',
    primary key (IDPUESTOS)
 );
 
@@ -277,8 +341,34 @@ create table REGISTRO_OBRAS
    FECHA_INICIO_OBRA    date  comment '',
    FECHA_FINAL_OBRA     date  comment '',
    STATUS_OBRA          varchar(50)  comment '',
-   COSTO                money  comment '',
+   COSTO                float  comment '',
    primary key (ID_OBRA)
+);
+
+/*==============================================================*/
+/* Table: REINTEGRO                                             */
+/*==============================================================*/
+create table REINTEGRO
+(
+   IDREINTEGRO          int not null auto_increment  comment '',
+   IDTRABAJADOR         int  comment '',
+   IDDETVIAT            int  comment '',
+   INGMONTO             float not null  comment '',
+   FECHAOTING           date not null  comment '',
+   DESCOTING            varchar(200) not null  comment '',
+   primary key (IDREINTEGRO)
+);
+
+/*==============================================================*/
+/* Table: SEGURO                                                */
+/*==============================================================*/
+create table SEGURO
+(
+   ID_SEGURO            varchar(50) not null  comment '',
+   CODIGO_EQUIPO        varchar(16)  comment '',
+   FECHA_VENCIDO        date  comment '',
+   COSTO_SEGURO         float  comment '',
+   primary key (ID_SEGURO)
 );
 
 /*==============================================================*/
@@ -287,9 +377,45 @@ create table REGISTRO_OBRAS
 create table SERVICIOS
 (
    IDSERVICIO           int not null auto_increment  comment '',
-   NOMBREAERVICIO       varchar(50) not null  comment '',
+   NOMBRESERVICIO       varchar(50) not null  comment '',
    DESCRIPCION          varchar(100) not null  comment '',
    primary key (IDSERVICIO)
+);
+
+/*==============================================================*/
+/* Table: SOLICITUDES_GASTOS                                    */
+/*==============================================================*/
+create table SOLICITUDES_GASTOS
+(
+   IDTRABAJADOR         int  comment '',
+   IDGASTO              int  comment '',
+   IDSOLICITUD          int not null auto_increment  comment '',
+   ESTADOSOLICITUD      char(1)  comment '',
+   primary key (IDSOLICITUD)
+);
+
+/*==============================================================*/
+/* Table: TIPOVIATICOS                                          */
+/*==============================================================*/
+create table TIPOVIATICOS
+(
+   IDTIVIATICO          int not null auto_increment  comment '',
+   NOMBRE               varchar(100) not null  comment '',
+   primary key (IDTIVIATICO)
+);
+
+/*==============================================================*/
+/* Table: TIPVIAT_VIATICOS                                      */
+/*==============================================================*/
+create table TIPVIAT_VIATICOS
+(
+   IDVIATICO            int  comment '',
+   IDTIVIATICO          int  comment '',
+   MONTOVIAT            float not null  comment '',
+   DESCVIAT             varchar(300) not null  comment '',
+   ESTADO               char(1) not null  comment '',
+   IDDETVIAT            int not null auto_increment  comment '',
+   primary key (IDDETVIAT)
 );
 
 /*==============================================================*/
@@ -340,8 +466,29 @@ create table VIAJE_INFORME_MUESTREO
    ID_INFORME_MUESTREO  int  comment ''
 );
 
-alter table CAT_PRESTACION add constraint FK_CAT_PRES_REFERENCE_PRESTACI foreign key (IDPRESTACION)
-      references PRESTACIONES (IDPRESTACION) on delete restrict on update restrict;
+/*==============================================================*/
+/* Table: VIATICOS                                              */
+/*==============================================================*/
+create table VIATICOS
+(
+   IDVIATICO            int not null auto_increment  comment '',
+   IDTRABAJADOR         int  comment '',
+   FECHAASIG            date not null  comment '',
+   MONTO                float not null  comment '',
+   primary key (IDVIATICO)
+);
+
+alter table CATALOGO_PRESUPUESTO add constraint FK_CATALOGO_REFERENCE_PRESUPUE foreign key (ID_PRESUPUESTO)
+      references PRESUPUESTO_OBRAS (ID_PRESUPUESTO) on delete restrict on update restrict;
+
+alter table COSTOMANTENER add constraint FK_COSTOMAN_REFERENCE_GASTOS foreign key (IDGASTO)
+      references GASTOS (IDGASTO) on delete restrict on update restrict;
+
+alter table COSTOMANTENER add constraint FK_COSTOMAN_REFERENCE_MANTENIM foreign key (ID_MANTENIMIENTO)
+      references MANTENIMIENTO (ID_MANTENIMIENTO) on delete restrict on update restrict;
+
+alter table COSTOMANTENER add constraint FK_COSTOMAN_REFERENCE_EQUIPO foreign key (CODIGO_EQUIPO)
+      references EQUIPO (CODIGO_EQUIPO) on delete restrict on update restrict;
 
 alter table EQUIPO_OBRA add constraint FK_EQUIPO_O_REFERENCE_EQUIPO foreign key (CODIGO_EQUIPO)
       references EQUIPO (CODIGO_EQUIPO) on delete restrict on update restrict;
@@ -367,10 +514,16 @@ alter table GASTOS_MATERIAL add constraint FK_GASTOS_M_REFERENCE_GASTOS foreign 
 alter table GASTOS_MATERIAL add constraint FK_GASTOS_M_REFERENCE_MATERIAL foreign key (ID_MATERIAL)
       references MATERIAL (ID_MATERIAL) on delete restrict on update restrict;
 
+alter table GASTOS_SEGURO add constraint FK_GASTOS_S_REFERENCE_GASTOS foreign key (IDGASTO)
+      references GASTOS (IDGASTO) on delete restrict on update restrict;
+
+alter table GASTOS_SEGURO add constraint FK_GASTOS_S_REFERENCE_SEGURO foreign key (ID_SEGURO)
+      references SEGURO (ID_SEGURO) on delete restrict on update restrict;
+
 alter table GASTOS_SERVICIO add constraint FK_GASTOS_S_REFERENCE_SERVICIO foreign key (IDSERVICIO)
       references SERVICIOS (IDSERVICIO) on delete restrict on update restrict;
 
-alter table GASTOS_SERVICIO add constraint FK_GASTOS_S_REFERENCE_GASTOS foreign key (IDGASTO)
+alter table GASTOS_SERVICIO add constraint FK_GASTOS_SS_REFERENCE_GASTOS foreign key (IDGASTO)
       references GASTOS (IDGASTO) on delete restrict on update restrict;
 
 alter table INCIDENTES add constraint FK_INCIDENT_REFERENCE_REGISTRO foreign key (ID_OBRA)
@@ -382,11 +535,17 @@ alter table INCIDENTES add constraint FK_INCIDENT_REFERENCE_TRABAJAD foreign key
 alter table INFORME_MUESTREO add constraint FK_INFORME__REFERENCE_REGISTRO foreign key (ID_OBRA)
       references REGISTRO_OBRAS (ID_OBRA) on delete restrict on update restrict;
 
-alter table INFORME_RESISTENCIAS add constraint FK_INFORME__REFERENCE_REGISTRO foreign key (ID_OBRA)
+alter table INFORME_RESISTENCIAS add constraint FK_INFORMEE__REFERENCE_REGISTRO foreign key (ID_OBRA)
       references REGISTRO_OBRAS (ID_OBRA) on delete restrict on update restrict;
 
 alter table INGRESOS add constraint FK_INGRESOS_REFERENCE_REGISTRO foreign key (ID_OBRA)
       references REGISTRO_OBRAS (ID_OBRA) on delete restrict on update restrict;
+
+alter table INGRESOS add constraint FK_INGRESOS_REFERENCE_TRABAJAD foreign key (IDTRABAJADOR)
+      references TRABAJADORES (IDTRABAJADOR) on delete restrict on update restrict;
+
+alter table MANTENIMIENTO add constraint FK_MANTENIM_REFERENCE_EQUIPO foreign key (CODIGO_EQUIPO)
+      references EQUIPO (CODIGO_EQUIPO) on delete restrict on update restrict;
 
 alter table MATERIAL_OBRA add constraint FK_MATERIAL_REFERENCE_MATERIAL foreign key (ID_MATERIAL)
       references MATERIAL (ID_MATERIAL) on delete restrict on update restrict;
@@ -396,6 +555,9 @@ alter table MATERIAL_OBRA add constraint FK_MATERIAL_REFERENCE_REGISTRO foreign 
 
 alter table MATERIAL_OBRA add constraint FK_MATERIAL_REFERENCE_TRABAJAD foreign key (IDTRABAJADOR)
       references TRABAJADORES (IDTRABAJADOR) on delete restrict on update restrict;
+
+alter table PRESTACIONES add constraint FK_PRESTACI_REFERENCE_CAT_PRES foreign key (IDCATPRESTACION)
+      references CAT_PRESTACION (IDCATPRESTACION) on delete restrict on update restrict;
 
 alter table PRESTACION_NOMINA_TRAB add constraint FK_PRESTACI_REFERENCE_PRESTACI foreign key (IDPRESTACION)
       references PRESTACIONES (IDPRESTACION) on delete restrict on update restrict;
@@ -415,6 +577,27 @@ alter table PRESUPUESTO_OBRAS add constraint FK_PRESUPUE_REFERENCE_REGISTRO fore
 alter table REGISTRO_OBRAS add constraint FK_REGISTRO_REFERENCE_TRABAJAD foreign key (IDTRABAJADOR)
       references TRABAJADORES (IDTRABAJADOR) on delete restrict on update restrict;
 
+alter table REINTEGRO add constraint FK_REINTEGR_REFERENCE_TRABAJAD foreign key (IDTRABAJADOR)
+      references TRABAJADORES (IDTRABAJADOR) on delete restrict on update restrict;
+
+alter table REINTEGRO add constraint FK_REINTEGR_REFERENCE_TIPVIAT_ foreign key (IDDETVIAT)
+      references TIPVIAT_VIATICOS (IDDETVIAT) on delete restrict on update restrict;
+
+alter table SEGURO add constraint FK_SEGURO_REFERENCE_EQUIPO foreign key (CODIGO_EQUIPO)
+      references EQUIPO (CODIGO_EQUIPO) on delete restrict on update restrict;
+
+alter table SOLICITUDES_GASTOS add constraint FK_SOLICITU_REFERENCE_TRABAJAD foreign key (IDTRABAJADOR)
+      references TRABAJADORES (IDTRABAJADOR) on delete restrict on update restrict;
+
+alter table SOLICITUDES_GASTOS add constraint FK_SOLICITU_REFERENCE_GASTOS foreign key (IDGASTO)
+      references GASTOS (IDGASTO) on delete restrict on update restrict;
+
+alter table TIPVIAT_VIATICOS add constraint FK_TIPVIAT__REFERENCE_VIATICOS foreign key (IDVIATICO)
+      references VIATICOS (IDVIATICO) on delete restrict on update restrict;
+
+alter table TIPVIAT_VIATICOS add constraint FK_TIPVIAT__REFERENCE_TIPOVIAT foreign key (IDTIVIATICO)
+      references TIPOVIATICOS (IDTIVIATICO) on delete restrict on update restrict;
+
 alter table TRABAJADORES add constraint FK_TRABAJAD_REFERENCE_PUESTOS foreign key (IDPUESTOS)
       references PUESTOS (IDPUESTOS) on delete restrict on update restrict;
 
@@ -423,4 +606,7 @@ alter table VIAJE_INFORME_MUESTREO add constraint FK_VIAJE_IN_REFERENCE_VIAJES f
 
 alter table VIAJE_INFORME_MUESTREO add constraint FK_VIAJE_IN_REFERENCE_INFORME_ foreign key (ID_INFORME_MUESTREO)
       references INFORME_MUESTREO (ID_INFORME_MUESTREO) on delete restrict on update restrict;
+
+alter table VIATICOS add constraint FK_VIATICOS_REFERENCE_TRABAJAD foreign key (IDTRABAJADOR)
+      references TRABAJADORES (IDTRABAJADOR) on delete restrict on update restrict;
 
