7ffb20e02850776b355035516e324111fcd4ef30
Luis <lumihernadezma@ittepic.edu.mx>
2021-06-13 19:07:17 -0700

    13/06/2021 Paul
    Se modifico tabla inferior y se cambiaron columnas de equipo

--- ModuloInventario/Modulo/php/InterfazInventario_Equipo/selectEquipo.php
+++ ModuloInventario/Modulo/php/InterfazInventario_Equipo/selectEquipo.php
@@ -1,7 +1,7 @@
 <?php
 $conexion = mysqli_connect("localhost","root","","inventario");
 if($conexion){
-    $consulta = "select E.CODIGO_EQUIPO,E.DESCRIPCION_EQUIPO,E.CARACTERISTICAS,E.MARCA_EQUIPO,E.MODELO_EQUIPO, E.TIPO_EQUIPO,M.ESTADO from Equipo E INNER JOIN Mantenimiento M ON E.CODIGO_EQUIPO = M.CODIGO_EQUIPO";
+    $consulta = "select E.CODIGO_EQUIPO,E.DESCRIPCION_EQUIPO,E.CARACTERISTICAS,E.MARCA_EQUIPO,E.MODELO_EQUIPO, E.TIPO_EQUIPO from Equipo E";
     $datos = $conexion->query($consulta);
         if($datos->num_rows>0){
             while($fila=$datos->fetch_assoc()){
@@ -11,7 +11,6 @@ if($conexion){
              $MARCA_EQUIPO =$fila['MARCA_EQUIPO'];
              $MODELO_EQUIPO=$fila['MODELO_EQUIPO'];
              $TIPO_EQUIPO=$fila['TIPO_EQUIPO'];
-             $ESTADO =$fila['ESTADO'];
              ?>
              <tr>
                  <td style="width : 100px"><?=$CofigoEquipo?></td>
@@ -19,7 +18,6 @@ if($conexion){
                  <td style="width : 70px"><?=$MARCA_EQUIPO?></td>
                  <td style="width : 80px"><?=$MODELO_EQUIPO?></td>
                  <td style="width : 60px"><?=$TIPO_EQUIPO?></td>
-                 <td style="width : 70px"><?=$ESTADO?></td>
                  <td style="width : 110px"><?=$descripcion?></td>
                  
                  
--- ModuloInventario/Modulo/php/InterfazInventario_Equipo/selectTabla.php
+++ ModuloInventario/Modulo/php/InterfazInventario_Equipo/selectTabla.php
@@ -1,21 +1,27 @@
 <?php
 $conexion = mysqli_connect("localhost","root","","inventario");
 if($conexion){
-    $consulta = "select E.CODIGO_EQUIPO,M.Estado,S.Fecha_Vencido from Equipo E 
+    $consulta = "select M.ID_MANTENIMIENTO,M.FECHA_PROX_M,S.ID_SEGURO,S.FECHA_VENCIDO,E.DESCRIPCION_EQUIPO from Equipo E 
     INNER JOIN Mantenimiento M ON E.CODIGO_EQUIPO = M.CODIGO_EQUIPO
     INNER JOIN Seguro S ON E.CODIGO_EQUIPO = S.CODIGO_EQUIPO";
     $datos = $conexion->query($consulta);
         if($datos->num_rows>0){
             while($fila=$datos->fetch_assoc()){
-             $CodigoEquipo =$fila['CODIGO_EQUIPO'];
-             $Estado =$fila['Estado'];
-             $FechaV =$fila['Fecha_Vencido'];
+            $Id_man = $fila['ID_MANTENIMIENTO'];
+            $fech_prox =$fila['FECHA_PROX_M'];
+            $Id_S = $fila['ID_SEGURO'];
+            $FechaV =$fila['FECHA_VENCIDO'];
+            $Descri =$fila['DESCRIPCION_EQUIPO'];
+             
              
              ?>
              <tr>
-                 <td style="width : 150px"><?=$CodigoEquipo?></td>
-                 <td style="width : 130px"><?=$Estado?></td>
+                 <td style="width : 150px"><?=$Id_man?></td>
+                 <td style="width : 150px"><?= $fech_prox?></td>
+                 <td style="width : 150px"><?=$Id_S?></td>
                  <td style="width : 150px"><?=$FechaV?></td>
+                 <td style="width : 130px"><?=$Descri?></td>
+                
                  
                  
                  
